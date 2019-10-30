039431ec40b6342ec720744370a880b80fcfe47d
Angel Barrientos <angel.barrientos@pagofacil.net>
2019-10-30 17:07:22 -0600

    Add new Entitie for Email

---
+++ src/Shared/Domain/EmailAddress.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+namespace PagoFacil\Gateway\Shared\Domain;
+
+use InvalidArgumentException;
+
+
+final class EmailAddress
+{
+    /** @var string $value */
+    private $value = null;
+
+    /**
+     * EmailAddress constructor.
+     * @param string $value
+     * @throws InvalidArgumentException
+     */
+    public function __construct(string $value)
+    {
+        $this->ensureIsValidEmail($value);
+        $this->value = $value;
+    }
+
+    /**
+     * @param string $value
+     * @throws InvalidArgumentException
+     */
+    private function ensureIsValidEmail(string $value): void
+    {
+        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException(sprintf(
+                'The email <%s> is not valid', $value
+            ));
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function value(): string
+    {
+        return $this->value;
+    }
+
+}
