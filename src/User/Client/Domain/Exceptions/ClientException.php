484a7625850484d7ded2ab24c4ef56bf2f6bf162
Angel Barrientos <angel.barrientos@pagofacil.net>
2019-10-31 16:38:37 -0600

    Added new exception

---
+++ src/User/Client/Domain/Exceptions/ClientException.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+namespace PagoFacil\Gateway\User\Client\Domain\Exceptions;
+
+use PagoFacil\Gateway\Shared\Domain\Error\DomainError;
+use Throwable;
+
+
+class ClientException extends DomainError
+{
+    const CLIENT_NOT_EXIST = 'client_not_exist';
+
+    /**
+     * InvalidUuidError constructor.
+     * @param string $message
+     * @param string $errorCode
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    protected function __construct(string  $message, string $errorCode, int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->errorMessage = $message;
+        $this->errorCode = $errorCode;
+    }
+
+    static public function notExist(string $errorMessage): self
+    {
+        return new static($errorMessage, static::CLIENT_NOT_EXIST);
+    }
+
+    public function errorCode(): string
+    {
+        return $this->errorCode;
+    }
+
+    public function errorMessage(): string
+    {
+        return $this->errorMessage;
+    }
+}
