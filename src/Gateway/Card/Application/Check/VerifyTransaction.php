3a532dd65c5cc05bf0b5660e2b6ba16a03594fa6
Angel Barrientos <angel.barrientos@pagofacil.net>
2019-11-08 18:25:12 -0600

    new implementation for use cases

--- src/Gateway/Card/Application/Check/VerifyTransaction.php
+++ src/Gateway/Card/Application/Check/VerifyTransaction.php
@@ -1,9 +1,53 @@
 <?php
 
+declare(strict_types=1);
+
 namespace PagoFacil\Gateway\Gateway\Card\Application\Check;
 
+use Psr\Log\LoggerInterface;
+use GuzzleHttp\ClientInterface;
+use League\Fractal\Resource\ResourceInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use PagoFacil\Gateway\Shared\Infrastructure\Interfaces\QueryRepository;
+use PagoFacil\Gateway\Shared\Infrastructure\Interfaces\CommandRepository;
 
 class VerifyTransaction
 {
+    /**
+     * @var QueryRepository $queryRepository
+     */
+    private $queryRepository;
+    /**
+     * @var CommandRepository $commandRepository
+     */
+    private $commandRepository;
+    /**
+     * @var ClientInterface $client
+     */
+    private $client;
+    /**
+     * @var LoggerInterface $logger
+     */
+    private $logger;
 
+    /**
+     * VerifyTransaction constructor.
+     * @param QueryRepository $queryRepository
+     * @param CommandRepository $commandRepository
+     * @param ClientInterface $client
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        QueryRepository $queryRepository,
+        CommandRepository $commandRepository,
+        ClientInterface $client,
+        LoggerInterface $logger
+    )
+    {
+        $this->queryRepository = $queryRepository;
+        $this->commandRepository = $commandRepository;
+        $this->client = $client;
+        $this->logger = $logger;
+    }
 }
--- src/Gateway/Card/Application/Create/CreateTransaction.php
+++ src/Gateway/Card/Application/Create/CreateTransaction.php
@@ -1,9 +1,68 @@
 <?php
+
 declare(strict_types=1);
+
 namespace PagoFacil\Gateway\Card\Application\Create;
 
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use League\Fractal\Resource\ResourceInterface;
+use PagoFacil\Gateway\Shared\Application\Transaction\Interfaces\SerializerAggregate;
+use PagoFacil\Gateway\Shared\Infrastructure\Interfaces\CommandRepository;
+use PagoFacil\Gateway\Shared\Infrastructure\Interfaces\QueryRepository;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
 
 class CreateTransaction
 {
+    /** @var ClientInterface $client  */
+    private $client = null;
+    /** @var LoggerInterface $logger */
+    private $logger = null;
+    /** @var QueryRepository $queryRepository */
+    private $queryRepository = null;
+    /** @var CommandRepository $commandRepository  */
+    private $commandRepository = null;
+    /** @var SerializerAggregate $serializerAggregate */
+    private $serializerAggregate;
 
+    /**
+     * CreateTransaction constructor.
+     * @param ClientInterface $client
+     * @param LoggerInterface $logger
+     * @param QueryRepository $queryRepository
+     * @param CommandRepository $commandRepository
+     * @param SerializerAggregate $serializerAggregate
+     */
+    public function __construct(
+        ClientInterface $client,
+        LoggerInterface $logger,
+        QueryRepository $queryRepository,
+        CommandRepository $commandRepository,
+        SerializerAggregate $serializerAggregate
+    )
+    {
+        $this->client = $client;
+        $this->logger = $logger;
+        $this->queryRepository = $queryRepository;
+        $this->commandRepository = $commandRepository;
+        $this->serializerAggregate = $serializerAggregate;
+    }
+
+    /**
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function sendTransaction(): ResponseInterface
+    {
+        return $this->client->request('POST', '/Wstransaccion/format/json?', [
+            'form_params' => [
+                'method' => '',
+                'data' => []
+            ]
+        ]);
+    }
+
+    protected function createResource(): ResourceInterface
+    {}
 }
